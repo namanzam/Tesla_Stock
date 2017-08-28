0ba89c5c2dd7f125e12114a4ab1d4f269afc7c59
Naman Mehra <naman2609@gmail.com>
2017-08-28 22:30:38 +0200

    e Please enter the commit message for your changes. Lines starting

--- tsla_data.py
+++ tsla_data.py
@@ -2,9 +2,10 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from plotly.offline import download_plotlyjs, iplot, plot, init_notebook_mode
 
 tesla = pd.read_csv('tsla.csv', index_col=0).drop('Volume', axis=1)
-time_plot = tesla.plot.line(x = tesla.index, y = 'Close', figsize = (12,4), lw=1)
+time_plot = tesla.plot.line(x=tesla.index, y = 'Close', figsize=(12, 4), lw=1)
 box_plot = tesla.plot.box()
 
 time_plot.figure.savefig('tesla_stock_time.png')

1110650d0488cb3a7f6ba7916ab49548a58a7900
Naman Mehra <naman2609@gmail.com>
2017-08-28 19:07:46 +0200

    Adding the rest of the files

---
+++ tsla_data.py
@@ -0,0 +1,11 @@
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+tesla = pd.read_csv('tsla.csv', index_col=0).drop('Volume', axis=1)
+time_plot = tesla.plot.line(x = tesla.index, y = 'Close', figsize = (12,4), lw=1)
+box_plot = tesla.plot.box()
+
+time_plot.figure.savefig('tesla_stock_time.png')
+box_plot.figure.savefig('tesla_stock_box.png')
